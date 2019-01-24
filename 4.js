0865a8fb87f91a2eaf1184e29499159db570269d
lintangwisesa <lintangwisesa@ymail.com>
2019-01-24 10:35:03 +0700

    mongodb

---
+++ 4.js
@@ -0,0 +1,17 @@
+var mongo = require('mongodb')
+var mongc = mongo.MongoClient
+var url = 'mongodb://lintang:1234@localhost:27017/tokoku'
+
+// connect then get insert a data
+mongc.connect(url, (error, client)=>{
+    console.log('Terhubung!')
+    var koleksi = client.db('tokoku').collection('users')
+    koleksi.insertOne({
+        nama: 'Fafa',
+        email: 'fafa@fafa.com',
+        kota: 'Jakarta'
+    }, () => {
+        console.log('Data terkirim!')
+        client.close()
+    })
+})
