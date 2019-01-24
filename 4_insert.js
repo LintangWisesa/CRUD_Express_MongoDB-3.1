6e172dc1d33870f804773d90ff83ceeacf38642b
lintangwisesa <lintangwisesa@ymail.com>
2019-01-24 10:57:33 +0700

    mongodb

--- 4_insert.js
+++ 4_insert.js
@@ -2,14 +2,15 @@ var mongo = require('mongodb')
 var mongc = mongo.MongoClient
 var url = 'mongodb://lintang:1234@localhost:27017/tokoku'
 
-// connect then get insert many
+// connect then insert a data
 mongc.connect(url, (error, client)=>{
     console.log('Terhubung!')
     var koleksi = client.db('tokoku').collection('users')
-    koleksi.insertMany([
-        {nama: 'Gilang', usia: 23},
-        {nama: 'Hamid', usia: 32, job: 'PNS', kota: 'Depok'}
-    ], () => {
+    koleksi.insertOne({
+        nama: 'Fafa',
+        email: 'fafa@fafa.com',
+        kota: 'Jakarta'
+    }, () => {
         console.log('Data terkirim!')
         client.close()
     })
--- 5_insertMany.js
+++ 5_insertMany.js
@@ -2,15 +2,14 @@ var mongo = require('mongodb')
 var mongc = mongo.MongoClient
 var url = 'mongodb://lintang:1234@localhost:27017/tokoku'
 
-// connect then get insert a data
+// connect then insert many
 mongc.connect(url, (error, client)=>{
     console.log('Terhubung!')
     var koleksi = client.db('tokoku').collection('users')
-    koleksi.insertOne({
-        nama: 'Fafa',
-        email: 'fafa@fafa.com',
-        kota: 'Jakarta'
-    }, () => {
+    koleksi.insertMany([
+        {nama: 'Gilang', usia: 23},
+        {nama: 'Hamid', usia: 32, job: 'PNS', kota: 'Depok'}
+    ], () => {
         console.log('Data terkirim!')
         client.close()
     })
