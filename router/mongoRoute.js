b7d6fd7bdcc1ba4157b6f1ae45327be215061ec2
lintangwisesa <lintangwisesa@ymail.com>
2019-01-24 11:54:41 +0700

    mongodb

--- router/mongoRoute.js
+++ router/mongoRoute.js
@@ -52,4 +52,42 @@ router.post('/data', (req, res)=>{
     })
 })
 
+// DELETE data by index
+router.delete('/data/:index', (req, res)=>{
+    mongc.connect(url, (error, client)=>{
+        console.log('Terhubung!')
+        var koleksi = client.db('tokoku').collection('users')
+        koleksi.find().toArray((error, data)=>{
+            koleksi.deleteOne({nama: data[req.params.index - 1].nama}, ()=>{
+                console.log('Data terhapus')
+                res.send('Data terhapus')
+                client.close()
+            })  
+        })
+    })
+})
+
+// PUT update datA BY INDEX ARRAY
+router.put('/data/:index', (req, res)=>{
+    mongc.connect(url, (error, client)=>{
+        console.log('Terhubung!')
+        var koleksi = client.db('tokoku').collection('users')
+        koleksi.find().toArray((error, data)=>{
+            koleksi.updateOne({
+                nama: data[req.params.index - 1].nama
+            }, {
+                $set: {
+                    nama: req.body.name,
+                    email: req.body.surel,
+                    kota: req.body.city
+                }
+            }, () => {
+                console.log('Data terupdate')
+                res.send('Data terupdate')
+                client.close()
+            })  
+        })
+    })
+})
+
 module.exports = router
