a0be3ab849938b997c11ebd5a9028eccbaf91c98
lintangwisesa <lintangwisesa@ymail.com>
2019-01-24 11:15:18 +0700

    mongodb

--- router/mongoRoute.js
+++ router/mongoRoute.js
@@ -1,3 +1,20 @@
 var router = require('express').Router()
+var bodyParser = require('body-parser')
+var mongc = require('mongodb').MongoClient
+var url = 'mongodb://lintang:1234@localhost:27017/tokoku'
+router.use(bodyParser.json())
+
+// GET all data
+router.get('/data', (req, res)=>{
+    mongc.connect(url, (error, client)=>{
+        console.log('Terhubung!')
+        var koleksi = client.db('tokoku').collection('users')
+        koleksi.find().toArray((error, data)=>{
+            console.log(data)
+            res.send(data)
+            client.close()
+        })
+    })
+})
 
 module.exports = router
